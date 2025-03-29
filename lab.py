f7f6da8e1e03778cd8d72f8e58032a5d19d09b29
orangesaft <156236994+orangesaft@users.noreply.github.com>
2025-03-28 23:28:45 -0700

    initial commit

---
+++ lab.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+import xgboost as xgb
+
+
+df = pd.read_csv("/Users/gwonjinlee/ds004302-download/participants.tsv", sep="\t")
+print(df.head())
+
+img = nib.load("/Users/gwonjinlee/ds004302-download/sub-01/anat/sub-01_T1w.nii.gz")
+data = img.get_fdata()
+print("Image shape:", data.shape)
+
+plt.imshow(data[:, :, data.shape[2] // 2].T, cmap="gray", origin="lower")
+plt.title("sub-01 (Mid Slice)")
+plt.axis("off")
+plt.show()
+
