c7d3ec16db3e1bf12758ebc4d02e7dfc8aedf4b6
orangesaft <156236994+orangesaft@users.noreply.github.com>
2025-03-29 17:28:06 -0700

    ready

--- lab.py
+++ lab.py
@@ -6,17 +6,17 @@ import xgboost as xgb
 import os
 
 df = pd.read_csv("/Users/gwonjinlee/ds004302-download/participants.tsv", sep="\t")
-print(df.head())
-
-
-img = nib.load("/Users/gwonjinlee/ds004302-download/sub-01/anat/sub-01_T1w.nii.gz")
-data = img.get_fdata()
-print("Image shape:", data.shape)
-
-plt.imshow(data[:, :, data.shape[2] // 2].T, cmap="gray", origin="lower")
-plt.title("sub-01 (Mid Slice)")
-plt.axis("off")
-plt.show()
+# #print(df.head())
+#
+#
+# img = nib.load("/Users/gwonjinlee/ds004302-download/sub-01/anat/sub-01_T1w.nii.gz")
+# data = img.get_fdata()
+# #print("Image shape:", data.shape)
+#
+# plt.imshow(data[:, :, data.shape[2] // 2].T, cmap="gray", origin="lower")
+# plt.title("sub-01 (Mid Slice)")
+# plt.axis("off")
+# #plt.show()
 
 
 df['psyrats'] = df['psyrats'].fillna(0)
@@ -29,16 +29,13 @@ for _, row in df.iterrows():
     psyrats_score = row['psyrats']
 
     try:
-        # Path to the subject's MRI
         path = f"/Users/gwonjinlee/ds004302-download/{subject_id}/anat/{subject_id}_T1w.nii.gz"
 
-        # Load and extract middle slice
         img = nib.load(path)
         data = img.get_fdata()
         mid_slice = data[:, :, data.shape[2] // 2]
         flat_slice = mid_slice.flatten()
 
-        # Append features and label
         x.append(flat_slice)
         y.append(psyrats_score)
 
