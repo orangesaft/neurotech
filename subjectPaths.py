9324bab486e59b2d0f7ab04cae8b01705930fae0
orangesaft <156236994+orangesaft@users.noreply.github.com>
2025-04-27 14:53:49 -0700

    change

--- subjectPaths.py
+++ subjectPaths.py
@@ -1,7 +1,12 @@
+from lab2 import run_model
 
 for i in range(1, 78):
     synthsegPath = "/Users/gwonjinlee/Downloads/MRI_Data_For_Project/sub-" + str(i) + "/sub-" + str(i) + "_T1w_synthseg.nii.gz"
     print('"' +synthsegPath+ '",')
+
+for i in range(1, 61):
+    run_model = "run_model(" + str(i) + ")"
+    print(run_model)
 
 
 
